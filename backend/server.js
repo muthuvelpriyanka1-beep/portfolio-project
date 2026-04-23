b423a342d81973001799088a6b23e74e44bcb90c
muthuvelpriyanka <muthuvelpriyanka1@gmail.com>
2026-04-23 17:43:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+mongoose.connect(process.env.MONGO_URI);
 
 const Project = mongoose.model("Project", {
   title: String,
