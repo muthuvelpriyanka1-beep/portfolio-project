7b287b10ef83736e013de064d2e75caaa479a623
muthuvelpriyanka <muthuvelpriyanka1@gmail.com>
2026-04-23 20:26:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const Project=mongoose.model('Project',{title:String,description:String});
 app.get('/api/projects',async(req,res)=>{res.json(await Project.find());});
